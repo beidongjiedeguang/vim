549a7472f906706936e948cd6550cbec30125108
kunyuan <kunyuany@163.com>
2021-10-21 10:51:01 +0800

    update

--- install-v.sh
+++ install-v.sh
@@ -3,13 +3,13 @@
 #  The file $PWD/v is copied from
 #+ https://github.com/rupa/v
 
-[[ -e ~/.fangjun.sh ]] || ( echo "~/.fangjun.sh does not exist!" && exit 0; )
+[[ -e ~/.kunyuan.sh ]] || ( echo "~/.kunyuan.sh does not exist!" && exit 0; )
 
-if ! grep "add_path" ~/.fangjun.sh &> /dev/null; then
+if ! grep "add_path" ~/.kunyuan.sh &> /dev/null; then
   echo "add_path should be a function!"
   exit 0
 else
-  echo "add_path $PWD" >> ~/.fangjun.sh
+  echo "add_path $PWD" >> ~/.kunyuan.sh
 fi
 
 echo "Done"
